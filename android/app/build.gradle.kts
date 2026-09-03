180263121905b6d2e28d0d6fe136a7871aceefb1
nuke4u2 <william.nicoll@gmail.com>
2026-09-03 18:57:27 -0400

    Pin Android NDK to plugin-compatible version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.example.lit_reader"
     compileSdk = 37
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
